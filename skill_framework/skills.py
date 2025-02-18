6f2ea6fd857d0f57d318a24799a7eff06476dd49
Alexander Dodson <alexander.dodson@answerrocket.com>
2025-02-18 13:54:29 -0500

    rename SkillParameters to SkillInput, add a utility method that creates a dataclass to hold argument values, set up some basic unit tests and add them to the workflow

--- skill_framework/skills.py
+++ skill_framework/skills.py
@@ -1,9 +1,10 @@
 import inspect
 import jinja2
+import keyword
 import os
-from pydantic import BaseModel, ConfigDict
-from types import SimpleNamespace
-from typing import Callable
+from dataclasses import make_dataclass, field
+from pydantic import BaseModel, ConfigDict, field_validator
+from typing import Callable, Literal
 from skill_framework.util import flexible_decorator
 
 
@@ -12,15 +13,49 @@ class FrameworkBaseModel(BaseModel):
 
 
 class SkillParameter(FrameworkBaseModel):
+    """
+    Used in the @skill decorator to define parameters for your skill.
+    Attributes:
+        name: the name of the parameter. this needs to be a valid python identifier, as these names are used to generate
+            a dataclass that will contain the parameter arguments when your skill is invoked.
+        constrained_to: limit the valid arguments to this parameter to a particular type
+        is_multi: if true, multiple arguments can be assigned to this parameter, and its value will always be a list
+        description: the parameter description that will appear in the UI
+        constrained_values: if set, limits the valid arguments to this parameter to those in this list
+    """
     name: str
-    constrained_to: str | None = None
+    constrained_to: Literal['metrics', 'dimensions', 'filters'] | None = None
     is_multi: bool = False
+    description: str | None = None
+    constrained_values: list[str] = field(default_factory=list)
+
+    @field_validator('name')
+    @classmethod
+    def validate_name(cls, v):
+        if not is_valid_parameter_name(v):
+            raise ValueError('Parameter name must be a valid python identifier')
+        return v
 
 
-class SkillParameters:
-    def __init__(self, assistant_id, **kwargs):
+def is_valid_parameter_name(name: str) -> bool:
+    return name.isidentifier() and not keyword.iskeyword(name)
+
+
+class SkillInput:
+    """
+    Container for context and parameter arguments passed into the skill. Recommended to create this object with
+    the Skill.create_input helper method,
+
+    Attributes:
+        assistant_id: the id of the assistant in which your skill is running
+        arguments: the arguments for your skill's parameters extracted from, for example, a chat interaction. This is a
+            generated dataclass at runtime, so you can use attribute-style access to refer to them. "empty" values will
+            be populated based on your declared parameters. f. ex, a list parameter for which no arguments were captured
+            will be initialized to an empty list.
+    """
+    def __init__(self, assistant_id, arguments):
         self.assistant_id = assistant_id
-        self.arguments = SimpleNamespace(**kwargs)
+        self.arguments = arguments
 
     def __str__(self):
         return str(self.__dict__)
@@ -41,9 +76,9 @@ class SkillOutput(FrameworkBaseModel):
 
 
 class Skill:
-    def __init__(self, fn: Callable[[SkillParameters], SkillOutput], name=None, description=None, parameters=None):
+    def __init__(self, fn: Callable[[SkillInput], SkillOutput], name=None, description=None, parameters=None):
         self.fn = fn
-        self.parameters = parameters or []
+        self.parameters: list[SkillParameter] = parameters or []
         self.name = name or fn.__name__
         self.description = description
         self.nodes = []
@@ -55,6 +90,30 @@ class Skill:
         n = Node(fn, skill=self)
         self.nodes.append(n)
         return n
+
+    def create_input(self, assistant_id=None, arguments: dict | None = None) -> SkillInput:
+        if not arguments:
+            arguments = {}
+        skill_arguments = _create_skill_arguments(self, **arguments)
+        return SkillInput(assistant_id=assistant_id, arguments=skill_arguments)
+
+
+def _create_skill_arguments(skill: Skill, **kwargs):
+    def field_type(p: SkillParameter):
+        return list[str] if p.is_multi else str | None
+
+    def parameter_field(p: SkillParameter):
+        return field(default_factory=list) if p.is_multi else field(default=None)
+
+    fields = [
+        (param.name, field_type(param), parameter_field(param))
+        for param in skill.parameters
+    ]
+    cls = make_dataclass(
+        'SkillArguments',
+        fields,
+    )
+    return cls(**kwargs)
 
 
 class Node:
@@ -69,9 +128,15 @@ class Node:
 
 
 @flexible_decorator
-def skill(fn: Callable[[SkillParameters], SkillOutput], name: str = None, parameters: list[SkillParameter] | None = None, description: str = None):
+def skill(fn: Callable[[SkillInput], SkillOutput], name: str = None, parameters: list[SkillParameter] | None = None, description: str = None):
     """
-    Marks a function as a skill entrypoint.
+    Marks a function as a skill entry point.
+    :param name: the name of the skill, will default to the function's name if not provided.
+    :param parameters: a list of SkillParameters
+    :param description: the description of the skill that will appear in the ui
+    :return: your skill function wrapped in a Skill class that wraps the metadata defined in the constructor and
+        provides utility methods. This class defines __call__, so it can still be used like a normal function for the
+        purposes of testing.
     """
     return Skill(fn, name=name, parameters=parameters, description=description)
 
