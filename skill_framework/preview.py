dd7adecbe3a36167dadc442868ab9dd4502065a7
Alexander Dodson <alexander.dodson@answerrocket.com>
2025-02-28 10:25:30 -0500

    small qol rework to writing viz previews: attempt to embed the layout json string as real json so that it can be read in the preview by a human, and also make sure to still write the layout content to the file even if the json is invalid so that the errors will be easier to spot

--- skill_framework/preview.py
+++ skill_framework/preview.py
@@ -1,5 +1,5 @@
-import os.path
-import datetime
+import json
+import os
 
 from skill_framework import SkillOutput
 
@@ -9,12 +9,21 @@ def preview_skill(skill, skill_output: SkillOutput):
     Writes skill template output to a file so that it can be seen by the preview app
     :param skill: the skill function
     :param skill_output: the output of the skill
-    :return:
     """
     path = f'.previews/{skill.fn.__name__}'
     if not os.path.exists(f'{path}'):
         os.makedirs(f'{path}', exist_ok=True)
     for idx, viz in enumerate(skill_output.visualizations):
         with open(f'{path}/viz-{idx}.json', 'w') as f:
-            f.write(viz.model_dump_json())
+            # this is not just calling model_dump_json so that the json embedded in the layout can be written out
+            # as json that can be read by a human, since this is for previewing outputs.
+            try:
+                viz_dict = viz.model_dump()
+                viz_dict['layout'] = json.loads(viz_dict['layout'])
+                f.write(json.dumps(viz_dict, indent=2))
+            except Exception:
+                # write it out even if invalid so the user can still review it, the preview server skips invalid layouts
+                print(f'{viz.title} contains invalid json')
+                f.write(viz.layout)
+
     print(f'Preview at localhost:8484/print/{skill.fn.__name__}')
