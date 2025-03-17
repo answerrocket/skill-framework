5781bea3ce3aa2e02f126cd04a6b4090aea2e3bd
Alexander Dodson <alexander.dodson@answerrocket.com>
2025-03-17 12:42:44 -0400

    add ExportData to __init__

--- skill_framework/__init__.py
+++ skill_framework/__init__.py
@@ -7,11 +7,12 @@ __all__ = [
     'ExitFromSkillException',
     'ParameterDisplayDescription',
     'SuggestedQuestion',
-    'SkillVisualization'
+    'SkillVisualization',
+    'ExportData',
 ]
 
 from skill_framework.skills import (skill, SkillInput, SkillParameter, SkillOutput, ExitFromSkillException,
-                                    ParameterDisplayDescription, SuggestedQuestion, SkillVisualization)
+                                    ParameterDisplayDescription, SuggestedQuestion, SkillVisualization, ExportData)
 from skill_framework.preview import preview_skill
 
 __version__ = '0.3.4'
