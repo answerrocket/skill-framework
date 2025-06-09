5672abbad072347729fbdbdb3e45560461c9571b
Crawford <crawford.nakayama@answerrocket.com>
2025-06-09 15:02:03 -0500

    update version

--- skill_framework/__init__.py
+++ skill_framework/__init__.py
@@ -19,4 +19,4 @@ from skill_framework.preview import preview_skill
 from skill_framework.layouts import wire_layout
 from skill_framework.resources import skill_resource_path
 
-__version__ = '0.3.11'
+__version__ = '0.3.12'
