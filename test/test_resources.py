cad125b0d091602c1ec754e9c3fdbd92b2aebf22
Alexander Dodson <alexander.dodson@answerrocket.com>
2025-10-02 15:15:43 -0400

    add filepath helpers for copilot and copilot skill resources

--- test/test_resources.py
+++ test/test_resources.py
@@ -1,4 +1,4 @@
-from skill_framework import skill_resource_path
+from skill_framework import skill_resource_path, copilot_resource_path, copilot_skill_resource_path
 
 
 def test_local_path():
@@ -11,3 +11,22 @@ def test_path_with_base(monkeypatch):
     path = skill_resource_path('test.txt')
     assert path == 'some_base_dir/resources/test.txt'
 
+
+def test_copilot_skill_resource(monkeypatch):
+    local_path = copilot_skill_resource_path('test.txt')
+    assert local_path == 'resources/test.txt'
+    monkeypatch.setenv('AR_COPILOT_SKILL_RESOURCE_PATH', 'some_base_dir')
+    path = copilot_skill_resource_path('test.txt')
+    assert path == 'some_base_dir/test.txt'
+    nested_path = copilot_skill_resource_path('dir/test.txt')
+    assert nested_path == 'some_base_dir/dir/test.txt'
+
+
+def test_copilot_resource(monkeypatch):
+    local_path = copilot_resource_path('test.txt')
+    assert local_path == 'resources/test.txt'
+    monkeypatch.setenv('AR_COPILOT_RESOURCE_PATH', 'some_base_dir')
+    path = copilot_resource_path('test.txt')
+    assert path == 'some_base_dir/test.txt'
+    nested_path = copilot_resource_path('dir/test.txt')
+    assert nested_path == 'some_base_dir/dir/test.txt'
