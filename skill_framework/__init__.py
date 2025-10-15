f22603c0f3d5505dcf2601aae348ebcdc8096563
Grant Barchard <grant.barchard@answerrocket.com>
2025-10-15 14:30:36 -0400

    bump version to 0.3.15

--- skill_framework/__init__.py
+++ skill_framework/__init__.py
@@ -21,4 +21,4 @@ from skill_framework.preview import preview_skill
 from skill_framework.layouts import wire_layout
 from skill_framework.resources import skill_resource_path, copilot_skill_resource_path, copilot_resource_path
 
-__version__ = '0.3.14'
+__version__ = '0.3.15'
