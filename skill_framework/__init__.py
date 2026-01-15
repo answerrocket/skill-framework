445364c34b1a6a282efed3ac9fae3df5ae4c759f
Matt Haidet <matt.haidet@answerrocket.com>
2026-01-15 14:42:33 -0500

    version to 0.3.18

--- skill_framework/__init__.py
+++ skill_framework/__init__.py
@@ -42,4 +42,4 @@ from skill_framework.preview import preview_skill
 from skill_framework.layouts import wire_layout
 from skill_framework.resources import skill_resource_path, copilot_skill_resource_path, copilot_resource_path
 
-__version__ = '0.3.17'
+__version__ = '0.3.18'
